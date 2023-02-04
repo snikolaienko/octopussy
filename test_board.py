f04dc7347a770d67ecc5ab443da14e7f55712dea
sofia <sofia.nikolaienko@gmail.com>
2023-02-04 21:09:53 +0200

    1. refactoring

--- test_board.py
+++ test_board.py
@@ -1,66 +1,83 @@
 import requests
-import random
 import json
+
+import variables
+import variables as v
 
 
 def test_create_valid_board():
-    token_value = "ATTA6da12a38b514097498c043608314e514a5deb8e52523e233dbff9fd7ef7a2100CCE2DFF3"
-    key_value = "bae166012ae2cf255c73d6d777c61db0"
     random_name = "board_0"
-    url = "https://api.trello.com/1/boards/"
 
     query_params = {
-        "key": key_value,
-        "token": token_value,
         "name": random_name
     }
-    se = requests.Session()
-    response = se.post(
-        url, params=query_params
+    query_params.update(variables.authorization)
+    response = requests.post(
+        variables.board_url, params=query_params
     )
     assert response.status_code == 200
 
 
 def test_prefs_card_covers_false():
-    token_value = "ATTA6da12a38b514097498c043608314e514a5deb8e52523e233dbff9fd7ef7a2100CCE2DFF3"
-    key_value = "bae166012ae2cf255c73d6d777c61db0"
-    random_name = "deflist1"
-    url = "https://api.trello.com/1/boards/"
+    # random_name = "deflist1"
+    # query_params = {
+    #     "name": random_name,
+    #     "prefs": {"prefs_cardCovers": False}
+    # }
+    # query_params.update(v.authorization)
+    #
+    # response = requests.post(
+    #     v.board_url, params=query_params
+    # )
+    # assert response.status_code == 200
+    # library = json.loads(response.text)  # parse string to dictionary
+    # board_id = library['id']
 
-    query_params = {
-        "key": key_value,
-        "token": token_value,
-        "name": random_name,
-        "prefs": {"prefs_cardCovers": False}
+    board_id = "63de3570d91006c1b0e83cd5"
+    response_get = get_board_by_id(board_id, 200)
+    # return_get = json.loads(response_get.text)  # parse string to dictionary
+    # actual_board_name = return_get['name']
+    # assert random_name == actual_board_name
+    #
+    # delete_board_by_id(board_id, 200)
 
-    }
 
-    response = requests.post(
-        url, params=query_params
-    )
-    library = json.loads(response.text) # parse string to dictionary
-    board_id = library['id']
-    board_name = library['name']
-    print(response.status_code)
-    assert response.status_code == 200
-    get_url = url + board_id
-    query_params = {
-        "key": key_value,
-        "token": token_value
-    }
+def get_board_by_id(board_id, status_code):
+    url = v.board_url + board_id
     headers = {
         "Accept": "application/json"}
-    response_get = requests.get(
-        get_url,
+    res = requests.get(
+        url,
         headers=headers,
-        params=query_params
+        params=v.authorization
     )
-    assert response_get.status_code == 200
+    assert res.status_code == status_code
+    return res
 
-    return_get = json.loads(response_get.text)  # parse string to dictionary
-    actual_board_name = return_get['name']
-    assert random_name == actual_board_name
-    requests.delete(
-         get_url,
-         params=query_params
+
+def delete_board_by_id(board_id, status_code):
+    url = v.board_url + board_id
+    res = requests.delete(
+        url,
+        params=v.authorization
     )
+    assert res.status_code == status_code
+
+def test_clone_board():
+    random_name = "board_to_be_cloned"
+
+    query_params = {
+        "name": random_name,
+        "prefs": {
+            "backgroundColor": "#4BBF6B",
+            "background": "lime",
+            "backgroundBottomColor": "#4BBF6B",
+            "backgroundTopColor": "#4BBF6B",
+        }
+    }
+    query_params.update(variables.authorization)
+    response = requests.post(
+        variables.board_url, params=query_params
+    )
+
+    assert response.status_code == 200
